5817317fd0ac4dce6c499c8e01b165c03333e44f
manuelsalgado <salgadopietrini7@gmail.com>
2022-07-08 16:40:54 +0100

    validate form inputs

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Box } from "@mui/material";
 import { DesktopDatePicker } from "@mui/x-date-pickers";
 import MenuItem from "@mui/material/MenuItem";
+import isAlpha from "validator/lib/isAlpha";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../redux/store";
 import {
@@ -18,11 +19,82 @@ import {
   StyledButton,
 } from "./form.styles";
 
+interface Error {
+  status: boolean;
+  message: string;
+}
+
+interface Errors {
+  name: Error;
+  surname: Error;
+  country: Error;
+  birthday: Error;
+}
+
 function Form() {
   const { name, surname, country, birthday } = useSelector(
     (state: RootState) => state.user
   );
   const dispatch = useDispatch();
+  const isDisabled =
+    name === "" || surname === "" || country === "" || birthday === null;
+
+  const [errors, setErrors] = useState<Errors>({
+    name: { status: false, message: "Name must be a word" },
+    surname: { status: false, message: "Surname must be a word" },
+    country: { status: false, message: "Country must be one of the list" },
+    birthday: { status: false, message: "Birthday must be a date" },
+  });
+
+  const handleSave = () => {
+    if (!isAlpha(name)) {
+      setErrors((state) => ({
+        ...state,
+        name: { ...state.name, status: true },
+      }));
+    }
+    if (!isAlpha(surname)) {
+      setErrors((state) => ({
+        ...state,
+        surname: { ...state.surname, status: true },
+      }));
+    }
+    if (!isAlpha(surname)) {
+      setErrors((state) => ({
+        ...state,
+        country: { ...state.country, status: true },
+      }));
+    }
+    if (isAlpha(name) && isAlpha(surname) && isAlpha(surname)) {
+      dispatch(setName(""));
+      dispatch(setSurname(""));
+      dispatch(setCountry(""));
+      dispatch(setBirthday(null));
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setErrors((state) => ({
+      ...state,
+      [e.target.name]: {
+        ...state[e.target.name as keyof Errors],
+        status: false,
+      },
+    }));
+    switch (e.target.name) {
+      case "name":
+        dispatch(setName(e.target.value));
+        break;
+      case "surname":
+        dispatch(setSurname(e.target.value));
+        break;
+      case "country":
+        dispatch(setCountry(e.target.value));
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <Box>
@@ -30,20 +102,32 @@ function Form() {
         <StyledStack direction="column" spacing={2}>
           <TextField
             label="Name"
+            name="name"
             value={name}
-            onChange={(e) => dispatch(setName(e.target.value))}
+            required
+            error={errors.name.status}
+            helperText={errors.name.status ? errors.name.message : ""}
+            onChange={handleChange}
           />
           <TextField
             label="Surname"
+            name="surname"
             value={surname}
-            onChange={(e) => dispatch(setSurname(e.target.value))}
+            required
+            error={errors.surname.status}
+            helperText={errors.surname.status ? errors.surname.message : ""}
+            onChange={handleChange}
           />
           <TextField
-            label="Countries"
+            label="Country"
+            name="country"
             select
             value={country}
+            required
+            error={errors.country.status}
+            helperText={errors.country.status ? errors.country.message : ""}
             defaultValue=""
-            onChange={(e) => dispatch(setCountry(e.target.value))}
+            onChange={handleChange}
           >
             <MenuItem value="Venezuela">Venezuela</MenuItem>
             <MenuItem value="Portugal">Portugal</MenuItem>
@@ -54,10 +138,26 @@ function Form() {
             inputFormat="dd/MM/yyyy"
             value={birthday}
             onChange={(e) => dispatch(setBirthday(e))}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                name="birthday"
+                required
+                error={errors.birthday.status}
+                helperText={
+                  errors.birthday.status ? errors.birthday.message : ""
+                }
+              />
+            )}
           />
           <StyledButtonContainer>
-            <StyledButton variant="outlined">Save</StyledButton>
+            <StyledButton
+              variant="outlined"
+              disabled={isDisabled}
+              onClick={handleSave}
+            >
+              Save
+            </StyledButton>
           </StyledButtonContainer>
         </StyledStack>
       </StyledContainer>
--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -3,25 +3,7 @@ import Alert from "@mui/material/Alert";
 import { format } from "date-fns";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../redux/store";
-
-export const getYearsAtNextBirthday = (birthdate: Date): number => {
-  const birthYear = +format(birthdate, "yyyy");
-  const birthMonth = +format(birthdate, "M");
-  const birthDay = +format(birthdate, "d");
-  const currentYear = +format(new Date(), "yyyy");
-  const currentMonth = +format(new Date(), "M");
-  const currentDay = +format(new Date(), "d");
-  const distanceYear = currentYear - birthYear;
-  const distanceMonth = currentMonth - birthMonth;
-  const distanceDay = currentDay - birthDay;
-  if (distanceMonth === 0 && distanceMonth === 0 && distanceDay === 0) {
-    return -distanceYear;
-  }
-  if (distanceMonth < 0 || (distanceMonth === 0 && distanceDay < 0)) {
-    return distanceYear;
-  }
-  return distanceYear + 1;
-};
+import { getYearsAtNextBirthday } from "../../utils/getYearsAtNextBirthday";
 
 function Greeting() {
   const { name, country, birthday } = useSelector(
